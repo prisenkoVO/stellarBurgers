ee841868bca6c03962082671e68d332b83ffe2fb
prisenkoVO <rsomotors@gmail.com>
2022-10-23 20:40:54 +0500

    add initialState for currentIngredient

--- src/services/reducers/current-ingredient.js
+++ src/services/reducers/current-ingredient.js
@@ -1,6 +1,21 @@
-import { ADD_INGREDIENT_TO_PREVIEW, DELETE_INGREDIENT_FROM_PREVIEW } from "../actions/current-ingredient";
+import { ADD_INGREDIENT_TO_PREVIEW, DELETE_INGREDIENT_FROM_PREVIEW } from '../actions/current-ingredient';
 
-export const currentIngredientReducer = (state = null, action) => {
+export const initialState = {
+    _id: '',
+    name: '',
+    type: '',
+    proteins: 0,
+    fat: 0,
+    carbohydrates: 0,
+    calories: 0,
+    price: 0,
+    image: '',
+    image_mobile: '',
+    image_large: '',
+    __v: 0
+};
+
+export const currentIngredientReducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_INGREDIENT_TO_PREVIEW:
             return {
